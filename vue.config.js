e18745d0907f6afdd34a43669c6f92600d4c0006
不喜丶不悲 <172635616@qq.com>
2024-06-11 02:54:23 +0000

    fix: codesandbox Invalid Host header

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const { defineConfig } = require('@vue/cli-service');
 
-const resolve = dir => path.join(__dirname, dir);
+const resolve = (dir) => path.join(__dirname, dir);
 
 module.exports = defineConfig({
   publicPath: '/',
   productionSourceMap: false,
   transpileDependencies: true,
+  devServer: {
+    // fix: codesandbox(Invalid Host header)
+    allowedHosts: ['localhost', '.csb.app'],
+  },
   configureWebpack: {},
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     // set src as @
     config.resolve.alias.set('@', resolve('src'));
   },
